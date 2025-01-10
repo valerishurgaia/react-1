7168e681cd0b7ab1c2f0c8c6dc93117593b35987
Valeri Shurgaia <valerishurgo@gmail.com>
2025-01-10 21:31:57 +0400

    main commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  let name = "valeri"
+
+  console.log("123")
+
+  return (
+    <>
+    <h1 className="">{name}</h1>
+    <h1>asdas</h1>
+    </>
+  )
+}
+
+export default App
